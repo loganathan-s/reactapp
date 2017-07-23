d5288adce3bc03805bfbcc21b64013c365b5b9de
Loganathan Sellappa <Loganathan.Sellappa@logmein.com>
2017-07-23 22:22:11 +0530

    Ejected from React app to customize webpack and other

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyCnPAAmai9El22OTeqygXtWjeQqXQy-zdE",
+    authDomain: "catchoftheday-4b12b.firebaseapp.com",
+    databaseURL: "https://catchoftheday-4b12b.firebaseio.com"
+});
+
+export default base;
